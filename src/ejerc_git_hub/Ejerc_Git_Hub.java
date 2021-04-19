bf9a2b2f964c929073d92885ae3c22c988c7d698
Juana Garcia Seañez <jjggsscia@gmail.com>
2021-04-19 14:51:51 -0600

    Programa despliega saludos, primer ejercicio

---
+++ src/ejerc_git_hub/Ejerc_Git_Hub.java
@@ -0,0 +1,20 @@
+/*
+ * Proyecto generado en Netbeans para trabajar programas que se puedan
+   visualizarlos en  Git Hub
+ */
+package ejerc_git_hub;
+
+/**
+ *
+ * @author jjggs_000
+ */
+public class Ejerc_Git_Hub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola soy JGS");
+    }
+    
+}
